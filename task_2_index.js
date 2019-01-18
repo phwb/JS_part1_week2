daaf685c652859a69164e54e6aa5094cb6c3e99d
MaksimSvyatov <44829756+MaksimSvyatov@users.noreply.github.com>
2019-01-18 12:59:51 +0300

    Update and rename task_2.js to task_2_index.js

--- task_2_index.js
+++ task_2_index.js
@@ -22,20 +22,3 @@ module.exports = function (hashtags) {
     var finalResult = result.join();
     console.log(finalResult);
 };
-
-checks.js
-
-// Встроенный в Node.JS модуль для проверок
-var assert = require('assert');
-
-// Подключаем свою функцию
-var normalizeHashTags = require('./index.js');
-
-assert.deepEqual(
-    normalizeHashTags(['web', 'coursera', 'JavaScript', 'Coursera', 'script', 'programming']),
-    'web, coursera, javascript, script, programming',
-    'Список "web, coursera, JavaScript, Coursera, script, programming"' +
-    ' содержит хэштеги "web, coursera, javascript, script, programming"'
-);
-
-console.info('OK!');
