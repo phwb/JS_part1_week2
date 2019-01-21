bec231b30c5dd0322c22afd10a663b30a9198bcb
MaksimSvyatov <44829756+MaksimSvyatov@users.noreply.github.com>
2019-01-21 23:14:43 +0300

    Update task_2_index.js
    Сделал, используя, indexOf и массив теперь перебирается слева-направо

--- task_2_index.js
+++ task_2_index.js
@@ -5,23 +5,18 @@ index.js
  * @returns {String}
  */
 module.exports = function (hashtags) {
-    var hashtags = ['web', 'coursera', 'JavaScript', 'Coursera', 'script', 'programming'];
-    var lowerCaseArr = [];
-    var temp = [];
-    var result = [];
+    var arr = [];
     for (var i=0; i<hashtags.length; i++) {
-        lowerCaseArr[i] = hashtags[i].toLowerCase();
+    arr[i] = hashtags[i].toLowerCase();
     }
-    for (i=lowerCaseArr.length-1; i>=0; i--) {
-        if (lowerCaseArr[i] in temp) continue;
-        result.push(lowerCaseArr[i]);
-        temp[lowerCaseArr[i]] = 1;
-    }
-    result = result.reverse();
-    var temp1 = result[2];
-    result[2] = result[1];
-    result[1] = temp1;
-    var finalResult = result.join(', ');
 
-    return finalResult;
+    var result = [];
+    for (var i=0; i<arr.length; i++) {
+    var a = arr[i];
+    if (result.indexOf(a) === -1) {
+      result.push(a);
+    }
+    }
+    result = result.join(', ');
+    return result;
 };
